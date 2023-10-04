78dcaa563f3dca59a4e2c746ddfaa03d9490e73e
unknown <david.nascimento@multiversoeducacao.com.br>
2023-10-04 14:37:42 -0300

    Incluido ao código o tratamento para criar uma planilha com a listagem completa dos alunos e outras auxiliares para gerar um arquivo em paralelo com a lista dos novos alunos de um dia para o outro

--- Base_diaria.py
+++ Base_diaria.py
@@ -2,6 +2,11 @@
 
 # Importar as bibliotecas necessárias
 import pandas as pd
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
 
 # Carrega os DataFrames dos arquivos Excel
 tratado_terceiro = pd.read_excel(r'C:\Users\Victor\Downloads\Tratado_terceiro.xlsx')
@@ -13,3 +18,40 @@ nomes_a_serem_adicionados = tratado_terceiro[~tratado_terceiro['NOME DO ALUNO_x'
 # Salva os nomes em um quinto arquivo
 quinto_arquivo = r'C:\Users\Victor\Downloads\Tratado_quinto.xlsx'
 nomes_a_serem_adicionados.to_excel(quinto_arquivo, index=False)
+
+# Enviar para o email
+# Configurações de e-mail
+email_de = 'bdmultiverso@gmail.com'  # Seu e-mail
+senha = 'Aristoteles@123'  # Sua senha
+email_para = 'david.nascimento@multiversoeducacao.com.br'  # E-mail do destinatário
+assunto = 'Testando o envio da tabela por email'
+mensagem = 'Eaiii cara, será que deu certo enviar?.'
+
+# Cria a mensagem de e-mail
+msg = MIMEMultipart()
+msg['From'] = email_de
+msg['To'] = email_para
+msg['Subject'] = assunto
+
+# Adiciona o corpo da mensagem
+msg.attach(MIMEText(mensagem, 'plain'))
+
+# Adiciona o arquivo como anexo
+with open(quinto_arquivo, "rb") as arquivo:
+    part = MIMEApplication(arquivo.read(), Name="Tratado_quinto.xlsx")
+
+part['Content-Disposition'] = f'attachment; filename="{quinto_arquivo}"'
+msg.attach(part)
+
+# Configura o servidor SMTP
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+
+# Efetua login no servidor
+server.login(email_de, senha)
+
+# Envia o e-mail
+server.sendmail(email_de, email_para, msg.as_string())
+
+# Fecha a conexão com o servidor SMTP
+server.quit()
